48ffd6d75ecd0b817c5c28992a3cc97242d98593
Pravin L <pravin_l@trimble.com>
2024-09-25 11:48:49 +0530

    Refactor siye-viewer component and update dependencies

---
+++ src/components/siye-viewer/readme.md
@@ -0,0 +1,10 @@
+# siye-viewer
+
+
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
--- src/components/siye-viewer/siye-viewer.tsx
+++ src/components/siye-viewer/siye-viewer.tsx
@@ -1,15 +1,42 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h, Host } from '@stencil/core';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 @Component({
   tag: 'siye-viewer',
-  styleUrl: 'siye-viewer.css',
+  styleUrl: '../../../node_modules/leaflet/dist/leaflet.css',
   shadow: true,
 })
 export class SiyeViewer {
+  private mapContainer: HTMLElement;
+
+  componentDidLoad() {
+    console.log('Component has been rendered');
+    if (this.mapContainer) {
+      const map = L.map(this.mapContainer, {
+        center: [13.1,80.2],
+        zoom: 10
+      });
+
+      const mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; ' + mapLink + ' Contributors',
+        maxZoom: 18,
+      }).addTo(map);
+
+      setTimeout(() => {
+        map.invalidateSize();
+      }, 0);
+    } else {
+      console.error('Map container is not defined');
+    }
+  }
+
   render() {
+    console.log('Rendering component');
     return (
       <Host>
-        <slot></slot>
+        <div ref={(el) => this.mapContainer = el as HTMLElement} style={{ height: '100%', width: '100%', margin :'0px' }}></div>
       </Host>
     );
   }
